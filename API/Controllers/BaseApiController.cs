45b2c45383cde5731c06a920cfb06fd7188579d6
Ramon96 <rfm1@live.nl>
2022-06-08 14:53:07 +0200

    End of section 2

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Reactivities.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseApiController : ControllerBase
+    {
+        
+    }
+}
